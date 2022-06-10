f1416cbfafbc2ee5097650ea8bccf25fbc194f76
seomy <muyear.seo@gmail.com>
2022-06-10 13:22:25 +0900

    master

---
+++ src/main/java/develop/homedevelop/ServletInitializer.java
@@ -0,0 +1,13 @@
+package develop.homedevelop;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HomeDevelopApplication.class);
+	}
+
+}
---
+++ src/test/java/develop/homedevelop/HomeDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package develop.homedevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
